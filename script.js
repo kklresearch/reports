4c8f96ae1fce08469181eee159f64bf4cac1d62d
kklresearch <sumanth@kisankraft.com>
2025-07-07 13:50:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   // The URL for the Google Apps Script Web App.
-  const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyMqiU98DnwpObORid1m7tqdg40bQcyTUOVRFpdZhS5mHga3ps9wsfrDPzsy0SipnEH/exec';
+  const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzY9waUd8gQmxI1FBytHLKV0tSTCQfV2b16uFiVh4OkRNShEGCceDK5VrxHgvj5kqfb/exec';
 
   // Get references to common elements
   const dailyForm = document.getElementById('daily-form');
